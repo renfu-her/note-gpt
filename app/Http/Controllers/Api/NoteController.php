ec14cff4fa7dc7131a1d3bc225da92ea2c91d860
Patrick Her <renfu.her@gmail.com>
2025-04-22 10:52:13 +0800

    在 NoteController 的 store 方法中新增調試輸出 dd($request->all())，以檢查上傳的請求資料，為後續的參數驗證提供輔助，提升開發過程中的除錯效率。

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -144,6 +144,8 @@ class NoteController extends Controller
     public function store(Request $request) 
     {
 
+        dd($request->all());
+        
         try {
             $validator = validator($request->all(), [
                 'folder_id' => 'required|exists:note_folders,id',
